ffad1ec856d1bd9ace60eb8cca377f63b6e5dff8
Eugene Dudkin <ZheDre1N@gmail.com>
2022-03-20 15:31:31 +0300

    header for settings table

---
+++ Ethnic/Library/Extensions/UIColor + Ext.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor + Ext.swift
+//  Ethnic
+//
+//  Created by Eugene Dudkin on 20.03.2022.
+//
+
+import UIKit
+
+extension UIColor {
+  static let ethnicMainBlackColor = UIColor.black
+  static let ethnicSecondaryBlueColor = UIColor.systemBlue
+}
