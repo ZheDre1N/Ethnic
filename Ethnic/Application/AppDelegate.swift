8688ae183605ce3affbabfd97968df437340e786
Eugene Dudkin <ZheDre1N@gmail.com>
2022-03-28 20:22:51 +0300

    Core Data added

--- Ethnic/Application/AppDelegate.swift
+++ Ethnic/Application/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -25,5 +26,30 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
       name: "Default Configuration",
       sessionRole: connectingSceneSession.role
     )
+  }
+
+  // MARK: - Core Data stack
+  lazy var persistentContainer: NSPersistentCloudKitContainer = {
+    let container = NSPersistentCloudKitContainer(name: "Ethnic")
+    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+      if let error = error as NSError? {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    })
+    return container
+  }()
+
+  // MARK: - Core Data Saving support
+
+  func saveContext () {
+    let context = persistentContainer.viewContext
+    if context.hasChanges {
+      do {
+        try context.save()
+      } catch {
+        let nserror = error as NSError
+        fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+      }
+    }
   }
 }
